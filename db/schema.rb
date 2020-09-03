9de922c7a914ec5b5ea8a8a7658c8433f10a98cc
Diego Stiehl <seccomiro@gmail.com>
2020-09-02 22:40:55 -0300

    Create Player model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_013337) do
+ActiveRecord::Schema.define(version: 2020_09_03_013618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2020_09_03_013337) do
   create_table "matches", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.bigint "match_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["match_id"], name: "index_players_on_match_id"
+    t.index ["user_id"], name: "index_players_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +41,6 @@ ActiveRecord::Schema.define(version: 2020_09_03_013337) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "players", "matches"
+  add_foreign_key "players", "users"
 end
---
+++ spec/factories/players.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :player do
+    match { nil }
+    user { nil }
+  end
+end
