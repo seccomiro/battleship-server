ffd5709145163284ce973d86226dda6c8be67702
Diego Stiehl <seccomiro@gmail.com>
2020-09-03 18:29:43 -0300

    Update MatchChannel (but not being used right now)

--- app/channels/match_channel.rb
+++ app/channels/match_channel.rb
@@ -1,12 +1,27 @@
 class MatchChannel < ApplicationCable::Channel
   def subscribed
-    match = Match.find(params[:match_id])
-    match.players.find(params[:user_id])
+    set_match_and_player
+
+    stream_from "match_#{@match.id}"
+    stream_from "player_#{@player.id}"
   rescue ActiveRecord::RecordNotFound
     reject
   end
 
+  def join(data)
+    player_id = data['player_id']
+
+    ActionCable.server.broadcast "player_#{player_id}",
+                                 message: 'Welcome to Battleship'
+  end
+
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+  end
+
+  private
+
+  def set_match_and_player
+    @match = Match.find(params[:match_id])
+    @player = @match.players.find_by_user_id!(params[:user_id])
   end
 end
--- spec/channels/match_channel_spec.rb
+++ spec/channels/match_channel_spec.rb
@@ -1,22 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe MatchChannel, type: :channel do
-  context 'subscribing' do
-    let(:user1) { create(:user, email: 'user1@user.com') }
-    let(:user2) { create(:user, email: 'user2@user.com') }
-    let(:match) { create(:match) }
-    let(:player1) { create(:player, match: match, user: user1) }
-    let(:player2) { create(:player, match: match, user: user2) }
+  # let(:user1) { create(:user, email: 'user1@user.com') }
+  # let(:user2) { create(:user, email: 'user2@user.com') }
+  # let(:match) { create(:match) }
+  # let(:player1) { create(:player, match: match, user: user1) }
+  # let(:player2) { create(:player, match: match, user: user2) }
 
-    it "successfully subscribes" do
-      subscribe user_id: player1.id, match_id: match.id
+  before(:all) do
+    @user1 = create(:user, email: 'user1@user.com')
+    @user2 = create(:user, email: 'user2@user.com')
+    @match = create(:match)
+    @player1 = create(:player, match: @match, user: @user1)
+    @player2 = create(:player, match: @match, user: @user2)
+
+    stub_connection
+  end
+
+  context 'subscribing' do
+    it "successfully subscribes to a match" do
+      subscribe user_id: @user1.id, match_id: @match.id
       expect(subscription).to be_confirmed
     end
 
     it 'tries to subscribe to a match that it is not part of' do
       another_match = create(:match)
 
-      subscribe user_id: player1.id, match_id: another_match.id
+      subscribe user_id: @user1.id, match_id: another_match.id
       expect(subscription).to be_rejected
     end
   end
