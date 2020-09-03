6a506f4a18be025f256c9bd88b7a0b9f67fe8a41
Diego Stiehl <seccomiro@gmail.com>
2020-09-02 22:22:41 -0300

    Create MatchChannel and connection_spec

---
+++ app/channels/match_channel.rb
@@ -0,0 +1,9 @@
+class MatchChannel < ApplicationCable::Channel
+  def subscribed
+    reject unless params[:user_id] == 1
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
