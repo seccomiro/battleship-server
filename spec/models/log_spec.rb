96913cbd0ddec513acd67b0b5b74c4bc516c1f13
Diego Stiehl <seccomiro@gmail.com>
2020-09-11 16:39:53 -0300

    Write model tests for Log, Match and Player

--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Log, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    create_match
+  end
+
+  describe '.create' do
+    context 'for himself' do
+      it 'logs a message' do
+        @my_player.logs.create(message: 'Log message')
+        logs = @my_player.logs.map(&:message)
+
+        expect(logs.count).to eq(1)
+        expect(logs).to include('Log message')
+      end
+
+      it 'logs several messages in order' do
+        messages = (0..2).map { |i| "Log message #{i}" }
+        messages.each { |message| @my_player.logs.create(message: message) }
+
+        expect(@my_player.logs.count).to eq(3)
+        messages.each_with_index do |message, i|
+          expect(@my_player.logs[i].message).to eq(message)
+        end
+      end
+    end
+
+    context 'for the opponent' do
+      it 'logs a message' do
+        @my_player.opponent.logs.create(message: 'Log message')
+        logs = @my_player.opponent.logs.map(&:message)
+
+        expect(logs.count).to eq(1)
+        expect(logs).to include('Log message')
+      end
+    end
+  end
+
+  describe '#to_s' do
+    it 'returns the right string value' do
+      log = build(:log)
+
+      expect(log.to_s).to eq(log.message)
+    end
+  end
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    create_match
+  end
+
+  describe '#joined' do
+    context 'when the first enrolled player joins first' do
+      it 'return the players in the right order' do
+        @my_player.join
+        @opponent_player.join
+        expect(@match.players.joined.first).to eq(@my_player)
+        expect(@match.players.joined.last).to eq(@opponent_player)
+      end
+    end
+
+    context 'when the first enrolled player joins last' do
+      it 'return the players in the right order' do
+        @opponent_player.join
+        @my_player.join
+        expect(@match.players.joined.first).to eq(@opponent_player)
+        expect(@match.players.joined.last).to eq(@my_player)
+      end
+    end
+  end
 end
