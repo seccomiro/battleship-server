6aa291471d4fb365c50fd8a9f814606164f771d5
Diego Stiehl <seccomiro@gmail.com>
2020-09-11 16:35:24 -0300

    Ensure match during log creation

--- app/models/log.rb
+++ app/models/log.rb
@@ -2,7 +2,15 @@ class Log < ApplicationRecord
   belongs_to :match
   belongs_to :player, optional: true
 
+  before_validation :ensure_data
+
   def to_s
     message
+  end
+
+  private
+
+  def ensure_data
+    self.match = player.match unless match
   end
 end
