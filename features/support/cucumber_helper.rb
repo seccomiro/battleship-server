e862d4390ae19a04d10958e2ba76634cb02e2b07
Diego Stiehl <seccomiro@gmail.com>
2020-09-22 11:10:39 -0300

    Adapt test helpers to new Board creation

--- features/support/cucumber_helper.rb
+++ features/support/cucumber_helper.rb
@@ -9,11 +9,11 @@ end
 def create_my_player(board_height: 10, board_width: 10)
   @my_user = create(:user, email: 'user1@user.com', name: 'User 1')
   @my_player = create(:player, match: @match, user: @my_user)
-  @my_player.create_board(cells: Board.generate(height: board_height, width: board_width))
+  @my_player.create_board(height: board_height, width: board_width)
 end
 
 def create_opponent_player(board_height: 10, board_width: 10)
   @opponent_user = create(:user, email: 'user2@user.com', name: 'User 2')
   @opponent_player = create(:player, match: @match, user: @opponent_user)
-  @opponent_player.create_board(cells: Board.generate(height: board_height, width: board_width))
+  @opponent_player.create_board(height: board_height, width: board_width)
 end
