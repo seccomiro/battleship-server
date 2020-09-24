f34faffe10f44e37c55f613058a414aead92f129
Diego Stiehl <seccomiro@gmail.com>
2020-09-24 17:20:12 -0300

    Move some RSpec code to match_creation_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -93,22 +93,4 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
-
-  def create_match(board_height: 10, board_width: 10)
-    @match = create(:match)
-    create_my_player(board_height: board_height, board_width: board_width)
-    create_opponent_player(board_height: board_height, board_width: board_width)
-  end
-
-  def create_my_player(board_height: 10, board_width: 10)
-    @my_user = create(:user, email: 'user1@user.com', name: 'User 1')
-    @my_player = create(:player, match: @match, user: @my_user)
-    @my_player.create_board(height: board_height, width: board_width)
-  end
-
-  def create_opponent_player(board_height: 10, board_width: 10)
-    @opponent_user = create(:user, email: 'user2@user.com', name: 'User 2')
-    @opponent_player = create(:player, match: @match, user: @opponent_user)
-    @opponent_player.create_board(height: board_height, width: board_width)
-  end
 end
