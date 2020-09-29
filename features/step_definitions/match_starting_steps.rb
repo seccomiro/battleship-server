58a1a1de757a121d2a8f0bbd317ebe1fdff60166
Diego Stiehl <seccomiro@gmail.com>
2020-09-29 14:38:23 -0300

    Add feature for match starting

---
+++ features/step_definitions/match_starting_steps.rb
@@ -0,0 +1,15 @@
+When('the match is asked to begin') do
+  @match.begin
+end
+
+Then('it should draw the starting player') do
+  @drawn_player = @match.player_playing
+end
+
+Then('the starting player should be one of the players attached to the match') do
+  expect(@drawn_player).to eq(@my_player).or(eq(@opponent_player))
+end
+
+Then('the status of the match should be set to being played') do
+  @match.being_played?
+end
