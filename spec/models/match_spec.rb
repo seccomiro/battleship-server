5abaf77b3034284d41a1e1328e6136bdb691ce7e
Diego Stiehl <seccomiro@gmail.com>
2020-09-29 21:31:30 -0300

    Add specs for player attaching

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,47 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
-  before(:each) do
-    create_match
+  context 'with full match previously created' do
+    before(:each) do
+      create_match(distribute_boats: true)
+    end
+
+    it { is_expected.to define_enum_for(:status).with([:created, :ready, :players_joined, :being_played]) }
+
+    describe '#players.joined' do
+      context 'when the first enrolled player joins first' do
+        it 'return the players in the right order' do
+          @my_player.join
+          @opponent_player.join
+          expect(@match.players.joined.first).to eq(@my_player)
+          expect(@match.players.joined.last).to eq(@opponent_player)
+        end
+      end
+
+      context 'when the first enrolled player joins last' do
+        it 'return the players in the right order' do
+          @opponent_player.join
+          @my_player.join
+          expect(@match.players.joined.first).to eq(@opponent_player)
+          expect(@match.players.joined.last).to eq(@my_player)
+        end
+      end
+    end
+
+    describe '#begin' do
+      context 'when the players have already joined the match and are ready to be played' do
+        it 'starts the match and return true' do
+          @opponent_player.join
+          @my_player.join
+
+          expect(@match.begin).to be(true)
+          expect(@match.started_at).to be <= DateTime.now
+        end
+      end
+
+      context 'when the players did not join or are not waiting to start playing the match' do
+        it 'raises a MatchStartingError' do
+          expect { @match.begin }.to raise_error(Battleship::MatchStartingError)
+        end
+      end
+    end
   end
 
-  it { is_expected.to define_enum_for(:status).with([:created, :ready, :players_joined, :being_played]) }
-
-  describe '#players.joined' do
-    context 'when the first enrolled player joins first' do
-      it 'return the players in the right order' do
-        @my_player.join
-        @opponent_player.join
-        expect(@match.players.joined.first).to eq(@my_player)
-        expect(@match.players.joined.last).to eq(@opponent_player)
-      end
+  describe '#attach_player' do
+    it 'responds' do
+      expect(subject).to respond_to(:attach_player)
     end
 
-    context 'when the first enrolled player joins last' do
-      it 'return the players in the right order' do
-        @opponent_player.join
-        @my_player.join
-        expect(@match.players.joined.first).to eq(@opponent_player)
-        expect(@match.players.joined.last).to eq(@my_player)
-      end
-    end
-  end
+    it 'pushed the player into players array' do
+      create_my_player
+      create_match(players: false)
 
-  describe '#begin' do
-    context 'when the players have already joined the match and are ready to be played' do
-      it 'starts the match and return true' do
-        @opponent_player.join
-        @my_player.join
+      @match.attach_player(@my_player)
 
-        expect(@match.begin).to be(true)
-        expect(@match.started_at).to be <= DateTime.now
-      end
-    end
-
-    context 'when the players did not join or are not waiting to start playing the match' do
-      it 'raises a MatchStartingError' do
-        expect { @match.begin }.to raise_error(Battleship::MatchStartingError)
-      end
+      expect(@match.players).to include(@my_player)
     end
   end
 end
