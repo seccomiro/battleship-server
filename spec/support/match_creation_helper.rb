d83395b2fcccdf0ae8b6614bd292ec2d696eb9e7
Diego Stiehl <seccomiro@gmail.com>
2020-09-29 21:30:10 -0300

    Allow Match factories to distribute boats

--- spec/support/match_creation_helper.rb
+++ spec/support/match_creation_helper.rb
@@ -1,7 +1,12 @@
-def create_match(board_height: 10, board_width: 10)
+def create_match(board_height: 10, board_width: 10, players: [:my, :opponent], distribute_boats: false)
   @match = create(:match)
-  create_my_player(board_height: board_height, board_width: board_width)
-  create_opponent_player(board_height: board_height, board_width: board_width)
+
+  create_my_player(board_height: board_height, board_width: board_width) if players == :my || (players.respond_to?(:include?) && players.include?(:my))
+  create_opponent_player(board_height: board_height, board_width: board_width) if players == :opponent || (players.respond_to?(:include?) && players.include?(:opponent))
+  if distribute_boats
+    distribute_my_boats if @my_player
+    distribute_opponent_boats if @opponent_player
+  end
 end
 
 def create_my_user
@@ -22,4 +27,16 @@ def create_opponent_player(board_height: 10, board_width: 10)
   create_opponent_user
   @opponent_player = create(:player, match: @match, user: @opponent_user)
   @opponent_player.create_board(height: board_height, width: board_width)
+end
+
+def distribute_my_boats
+  @my_player.board.boats.order(size: :desc).each_with_index do |boat, i|
+    boat.place(:vertical, column: i, row: 0)
+  end
+end
+
+def distribute_opponent_boats
+  @opponent_player.board.boats.order(size: :desc).each_with_index do |boat, i|
+    boat.place(:vertical, column: i, row: 0)
+  end
 end
