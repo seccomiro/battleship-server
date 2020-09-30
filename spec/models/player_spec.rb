313a6b0140c76d0fd731c795ae5767075adeb40d
Diego Stiehl <seccomiro@gmail.com>
2020-09-29 21:31:07 -0300

    Add specs for player.valid?

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,43 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  before(:each) do
-    create_match
-  end
+  context 'with full match previously created' do
+    before do
+      create_match(distribute_boats: true)
+    end
 
-  describe '#opponent' do
-    it 'knows its opponent' do
-      expect(@my_player.opponent).to eq(@opponent_player)
+    describe '#opponent' do
+      it 'knows its opponent' do
+        expect(@my_player.opponent).to eq(@opponent_player)
+      end
+    end
+
+    describe '#join' do
+      it 'can join a match' do
+        expect(@my_player.join).to be(true)
+      end
+
+      it 'is the first player to join a match' do
+        @my_player.join
+        expect(@match.players.joined.count).to eq(1)
+        expect(@match.players.joined.first).to eq(@my_player)
+      end
+
+      it 'is the last player to join a match' do
+        @opponent_player.join
+        @my_player.join
+        expect(@match.players.count).to eq(2)
+        expect(@match.players.joined.first).to eq(@opponent_player)
+        expect(@match.players.joined.last).to eq(@my_player)
+      end
+    end
+
+    describe '#full_logs' do
+      it 'returns a list of strings with the correct messages' do
+        messages = (0..2).map { |i| "Log message #{i}" }
+        messages.each { |message| @my_player.logs.build(message: message) }
+
+        messages.each_with_index do |message, i|
+          expect(@my_player.full_logs[i]).to eq(message)
+        end
+      end
     end
   end
 
-  describe '#join' do
-    it 'can join a match' do
-      expect(@my_player.join).to be(true)
+  describe '#valid?' do
+    context 'when attached to a match' do
+      before(:each) do
+        create_match
+      end
+
+      context 'the board is not mounted' do
+        it 'is not valid' do
+          expect(@my_player).not_to be_valid
+        end
+      end
+
+      context 'the board is mounted' do
+        it 'is valid' do
+          distribute_my_boats
+
+          expect(@my_player).to be_valid
+        end
+      end
     end
 
-    it 'is the first player to join a match' do
-      @my_player.join
-      expect(@match.players.joined.count).to eq(1)
-      expect(@match.players.joined.first).to eq(@my_player)
-    end
+    context 'when not yet attached to a match' do
+      before(:each) do
+        create_my_player
+      end
 
-    it 'is the last player to join a match' do
-      @opponent_player.join
-      @my_player.join
-      expect(@match.players.count).to eq(2)
-      expect(@match.players.joined.first).to eq(@opponent_player)
-      expect(@match.players.joined.last).to eq(@my_player)
-    end
-  end
+      it 'is valid' do
+        expect(@my_player).to be_valid
+      end
 
-  describe '#full_logs' do
-    it 'returns a list of strings with the correct messages' do
-      messages = (0..2).map { |i| "Log message #{i}" }
-      messages.each { |message| @my_player.logs.build(message: message) }
+      context 'after attaching player to an existing match' do
+        before(:each) do
+          create_match(players: false)
+        end
 
-      messages.each_with_index do |message, i|
-        expect(@my_player.full_logs[i]).to eq(message)
+        context 'the board is not mounted' do
+          it 'is not valid' do
+            @match.attach_player(@my_player)
+
+            expect(@my_player).not_to be_valid
+          end
+        end
+
+        context 'the board is mounted' do
+          it 'is valid' do
+            distribute_my_boats
+            @match.attach_player(@my_player)
+
+            expect(@my_player).to be_valid
+          end
+        end
       end
     end
   end
